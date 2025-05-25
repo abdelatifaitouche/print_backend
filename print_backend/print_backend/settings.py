430304aae6ebace6d23c11ece5031fcda036fac7
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-05-25 15:31:41 +0200

    feat : skill issue

--- print_backend/print_backend/settings.py
+++ print_backend/print_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oc2w_f7mffwlaqpn5@8u7$ln_^r7nh1x2535dp4sv9ad9wyw_s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*" , "http://20.102.97.162:8000"]
+ALLOWED_HOSTS = ["*" , "http://20.102.97.162" , "localhost" , "127.0.0.1"]
 
 
 # Application definition
