3ecd7de0c28415664a716a1e0fa19b977390d8de
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2025-05-25 15:36:26 +0200

    fucking skill issue

--- print_backend/print_backend/settings.py
+++ print_backend/print_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oc2w_f7mffwlaqpn5@8u7$ln_^r7nh1x2535dp4sv9ad9wyw_s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*" , "http://20.102.97.162" , "localhost" , "127.0.0.1"]
+ALLOWED_HOSTS = ["*", "20.102.97.162", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -219,14 +219,14 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://print-frontend-juee.vercel.app",
-    "http://localhost:5173/"# ✅ ADD THIS!,
+    "http://localhost:5173" , # ✅ ADD THIS!
     "http://20.102.97.162:8000"
 ]
 
 SESSION_COOKIE_SAMESITE = "None"
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
 
 CSRF_COOKIE_SAMESITE = "None"
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 
 #THIS IS HJSUT COMMENTS
